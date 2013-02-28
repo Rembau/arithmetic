9d33b5b4d1833e8cd6e67028edb86c404556f6c2
Rembau <Rembau@Rembau-PC>
2013-02-28 17:05:43 +0800

    快速排序升级，for循环版优化

--- src/quick/QuickRank.java
+++ src/quick/QuickRank.java
@@ -4,14 +4,19 @@ import gainData.GainData;
 
 public class QuickRank {
 	static long startTime;
-	static long endTime;
-
+	static long endTime;;
+	static int c1=0;
+	static int c1_=0;
+	
+	static int c2=0;
+	static int c2_=0;
+	
 	public QuickRank() {
 	}
 
 	public void quickSort(int[] data, int low, int high) {
 		if (low < high) {
-			int pivot = parttion(data, low, high);
+			int pivot = partition(data, low, high);
 
 			quickSort(data, low, pivot - 1);
 			quickSort(data, pivot + 1, high);
@@ -19,27 +24,23 @@ public class QuickRank {
 
 	}
 
-	public int parttion(int[] data, int low, int high) {
-		int border = low;
+	public int partition(int[] data, int low, int high) {
+		int border = low;   //border记录当前比pivot小的数字的边界，找到一个比pivot小的就跟border交换，并且border加一
 		int pivot = data[low];
 		int temp = 0;
 		for (int i = low + 1; i <= high; i++) {
+			c1++;
+			c1_+=2;
 			if (data[i] < pivot) {
-				temp = data[border + 1];
-				data[border + 1] = data[i];
+				c1++;
+				temp = data[border];
+				data[border] = data[i];
 				data[i] = temp;
+				c1_+=4;
 				border++;
-				/*
-				 * System.out.print("排序"); for(int n: data){
-				 * System.out.print("  "+n); }
-				 */
-				// System.out.println(border);
 			}
 		}
-		temp = pivot;
-		data[low] = data[border];
-		data[border] = temp;
-		return border;
+		return border--;
 	}
 
 	/**
@@ -59,46 +60,55 @@ public class QuickRank {
 			i = low;
 			j = high;
 			x = data[i];
+			c2++;
+			c2_+=3;
 			while (i < j) {
 				while (i < j && data[j] > x) {
 					j--; // 从右向左找第一个小于x的数
+					c2_+=1;
 				}
 				if (i < j) {
 					data[i] = data[j];
 					i++;
+					c2_+=2;
 				}
 				while (i < j && data[i] < x) {
 					i++; // 从左向右找第一个大于x的数
+					c2_+=1;
 				}
 				if (i < j) {
 					data[j] = data[i];
 					j--;
+					c2_+=2;
 				}
+				c2+=7;
+				
 			}
 			data[i] = x;
 			qsort_asc(data, low, i - 1);
 			qsort_asc(data, i + 1, high);
+			c2_+=3;
 		}
 
 	}
 
 	public static void main(String args[]) {
 		QuickRank quick = new QuickRank();
-		GainData.setSize(100000);
+		GainData.setSize(10000);
 		int[] num = GainData.randomNum();
+
 		System.out.println(num[0]);
-		System.out.print("排序前");
-		/*
-		 * for(int n: num){ System.out.print("  "+n); }
-		 */
+		//System.out.println("排序前");
+		//for(int n: num){ System.out.println("  "+n); }
+
 		startTime = System.currentTimeMillis();
 		quick.quickSort(num, 0, num.length - 1);
-		// quick.qsort_asc(num,0,num.length-1);
+		quick.qsort_asc(num,0,num.length-1);
 		endTime = System.currentTimeMillis();
-		System.out.println("排序后");
-		/*
-		 * for(int n: num){ System.out.println(" "+n); }
-		 */
+		System.out.println("c1="+c1+" c1_="+c1_+" c2="+c2+" c2_="+c2_);
+		//System.out.println("排序后");
+		//for(int n: num){ System.out.println(" "+n); }
+		
 		System.out.println("====快速排序结束====");
 		System.out.println("快速排序用时:" + (endTime - startTime));
 	}
